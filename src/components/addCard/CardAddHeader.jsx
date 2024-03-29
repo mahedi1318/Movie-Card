21a73dd7644040100ed31db1dc28d3d4c0837ff3
mahedi1318 <mahedi.mern@gmail.com>
2024-03-30 01:09:33 +0600

    movie card all complite

--- src/components/addCard/CardAddHeader.jsx
+++ src/components/addCard/CardAddHeader.jsx
@@ -18,7 +18,7 @@ const CardAddHeader = ({onClose}) => {
       className="fixed top-0 left-0 w-screen h-screen z-50 bg-black/60 backdrop-blur-sm"
     >
       <div
-        className="absolute text-black left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-full max-w-[420px] sm:max-w-[600px] lg:max-w-[790px] p-4 max-h-[90vh] overflow-auto"
+        className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-full max-w-[420px] sm:max-w-[600px] lg:max-w-[790px] p-4 max-h-[90vh] overflow-auto"
       >
         <div
           className="bg-white shadow-md dark:bg-[#12141D] rounded-2xl overflow-hidden p-5 md:p-9"
--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Header from './Header'
 import Footer from './Footer'
 import LeftSidebar from './LeftSidebar'
 import MainBoard from '../movie/MainBoard'
+import { ThemContext } from '../../context/Context'
 
 const HomePage = () => {
+  const {darkMode} = useContext(ThemContext)
   return (
     <>
+    <div className={`${darkMode ? "dark" : ""}`}>
       <Header/>
       <main>
         <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
@@ -15,6 +18,7 @@ const HomePage = () => {
         </div>
       </main>
       <Footer/>
+      </div>
     </>
   )
 }
